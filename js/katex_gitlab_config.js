8f3f39e9b30a38004ba4eeb6056af7c65da28d3a
h@di <hadi@hadisafari.ir>
2018-07-02 19:59:01 +0430

    move centring full-line math formulas from js to stylesheet

--- js/katex_gitlab_config.js
+++ js/katex_gitlab_config.js
@@ -5,7 +5,6 @@ var katexMath = (function () {
     var maths = document.querySelectorAll('.js-render-math');
     for (var i = 0; i < maths.length; i++) {
       katex.render(maths[i].textContent || maths[i].innerText, maths[i], {'displayMode': false});
-      maths[i].className += " center";
     }
 });
 
